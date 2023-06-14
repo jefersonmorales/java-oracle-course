96ce5760c46665d34bb32285983c68140c19ab77
jefersonmorales-l <jefersmorales@gmail.com>
2023-06-14 16:30:51 -0600

    herencia

--- bytebank2(herencia e interfaces)/src/Gerente.java
+++ bytebank2(herencia e interfaces)/src/Gerente.java
@@ -1,28 +1,16 @@
-public class Gerente {
-
-  private String nombre;
-  private String documento;
-  private double salario;
-  public String getNombre() {
-    return nombre;
-  }
-  public void setNombre(String nombre) {
-    this.nombre = nombre;
-  }
-  public String getDocumento() {
-    return documento;
-  }
-  public void setDocumento(String documento) {
-    this.documento = documento;
-  }
-  public double getSalario() {
-    return salario;
-  }
-  public void setSalario(double salario) {
-    this.salario = salario;
+public class Gerente extends Funcionario{
+  
+  private String clave;
+  
+  public void setClave(String clave) {
+    this.clave = clave;
   }
 
-  public double getBonificacion() {
-    return this.salario * 1;
+  public String getClave() {
+    return clave;
+  }
+
+  public boolean iniciarSesion(String clave) {
+    return clave == "AluraCurso";
   }
 }
--- bytebank2(herencia e interfaces)/src/PruebaGerente.java
+++ bytebank2(herencia e interfaces)/src/PruebaGerente.java
@@ -1,13 +1,15 @@
 public class PruebaGerente {
 
   public static void main(String[] args) {
-    Funcionario gerente = new Funcionario();
+    Gerente gerente = new Gerente();
+    // Funcionario gerente = new Funcionario();
 
-    gerente.setSalario(600);
-    gerente.setTipo(1);
+    gerente.setClave("jefer");
     
-    System.out.println(gerente.getBonificacion());
-    System.out.println("su sueldo es: "+ (gerente.getSalario() + gerente.getBonificacion()));
+    // gerente.setSalario(600);
+    // gerente.setTipo(1);
+    
+    System.out.println(gerente.iniciarSesion(gerente.getClave()));
     
   }
   
