6ee8653600e6af824285810d6e9c54ede58bc9aa
jefersonmorales-l <jefersmorales@gmail.com>
2023-06-15 14:39:27 -0600

    modificando estructura

--- bytebank2(herencia e interfaces)/src/Gerente.java
+++ bytebank2(herencia e interfaces)/src/Gerente.java
@@ -1,21 +1,7 @@
-public class Gerente extends Funcionario{
-  
-  private String clave;
-  
-  public void setClave(String clave) {
-    this.clave = clave;
-  }
-
-  public String getClave() {
-    return clave;
-  }
-
-  public boolean iniciarSesion(String clave) {
-    return clave == "AluraCurso";
-  }
+public class Gerente extends FuncionarioAutenticable{
 
   public double getBonificacion() {
     System.out.println("EJECUTANDO DESDE GERENTE");
-    return super.getSalario()+(this.getSalario()*0.05);
+    return 2000;
   }
 }
---
+++ bytebank2(herencia e interfaces)/src/SistemaInterno.java
@@ -0,0 +1,17 @@
+public class SistemaInterno {
+
+  private String clave = "AluraCurso";
+
+  public boolean autenticar(FuncionarioAutenticable gerente) {
+    boolean puedeIniciarSesion = gerente.iniciarSesion(clave);
+
+    if(puedeIniciarSesion) {
+      System.out.println("Login exitoso");
+      return true;
+    } else {
+      System.out.println("Error en login");
+      return false;
+    }
+  }
+  
+}
