f96715aa599ff36b3ae959fd4fd05e033387887f
jefersonmorales-l <jefersmorales@gmail.com>
2023-06-19 16:10:01 -0600

    Controlando errores

---
+++ java-pila-ejecucion/src/Flujo.java
@@ -0,0 +1,44 @@
+public class Flujo {
+
+  public static void main(String[] args) {
+    System.out.println("Ini do main");
+    metodo1();
+    System.out.println("Fim do main");
+  }
+  
+  public static void metodo1(){
+    System.out.println("Ini do metodo1");
+    metodo2();
+    System.out.println("Fim do metodo1");
+  }
+
+  public static void metodo2(){
+    System.out.println("Ini do metodo2");
+  
+    for(int i =1; i<= 5; i++){
+      System.out.println(i);
+
+      try {
+        int num = 0;
+        int resultado = 20 / num; // Esto es un error porque cualquier numero dividido entre 0 es un error.
+        System.out.println(resultado);
+
+        String test = null;
+        System.out.println(test.toString());
+
+
+        // Podemos capturar diferentes errores, haciendo diferentes catch
+
+        // | Nos sirve para integrar diferentes errores
+      } catch(ArithmeticException | NullPointerException exception) { // Esto permite capturar el error
+        System.out.println("Atrapo error: " + exception.getMessage()); // nos retorna el mensaje
+        exception.printStackTrace(); // nos retorna la direccion de error
+      } 
+
+    }
+    
+    System.out.println("Fim do metodo2");
+  
+  }
+  
+}
