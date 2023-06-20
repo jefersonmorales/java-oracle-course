e2b100e9cec0ceeb2b91d5dd2cd126d8eeb52729
jefersonmorales-l <jefersmorales@gmail.com>
2023-06-20 13:28:23 -0600

    atrapando cualquier excepcion

--- java-pila-ejecucion/src/Flujo.java
+++ java-pila-ejecucion/src/Flujo.java
@@ -1,8 +1,14 @@
 public class Flujo {
 
-  public static void main(String[] args) throws MiException {
+  public static void main(String[] args) {
     System.out.println("Ini do main");
-    metodo1();
+
+    try {
+      metodo1();
+    } catch (Exception e) { //estamos atrapando cualquier excepcion
+      e.printStackTrace();
+    }
+    
     System.out.println("Fim do main");
   }
   
@@ -17,11 +23,4 @@ public class Flujo {
     System.out.println("Ini do metodo2");
     throw new MiException("Mi excepcion fue lanzada"); 
   }
-
-  
-
-  // En este caso estamos extendiendo directamente a Exception y es para asegurar que va a lanzar una excepcion, mientras que extendiendo con RuntimeException es indicar que puede ser posible que lance una excepcion. 
-
-  // si no queremos usar el metodo de try y catch() debemos indicar nuestra excepcion en cada uno con throws
-  
 }
