725dda392e84935643b5f50e977c3d559b697053
jefersonmorales-l <jefersmorales@gmail.com>
2023-06-20 14:49:54 -0600

    TestCuenta fixed

--- bytebank2(herencia e interfaces)/src/TestCuenta.java
+++ bytebank2(herencia e interfaces)/src/TestCuenta.java
@@ -6,7 +6,11 @@ public class TestCuenta {
     CuentaAhorros ca = new CuentaAhorros(2);
 
     cc.depositar(100);
-    cc.transfiere(80, ca);
+    try {
+      cc.transfiere(80, ca);
+    } catch (SaldoInsuficienteException e) {
+      e.printStackTrace();
+    }
 
     System.out.println(cc.getSaldo());
     System.out.println(ca.getSaldo());
