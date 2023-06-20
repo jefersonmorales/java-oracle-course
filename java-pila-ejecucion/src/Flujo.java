3ce9a51e454439f6367f7bbbb01e9be55a612244
jefersonmorales-l <jefersmorales@gmail.com>
2023-06-20 12:33:24 -0600

    creando una excepcion

--- java-pila-ejecucion/src/Flujo.java
+++ java-pila-ejecucion/src/Flujo.java
@@ -8,15 +8,19 @@ public class Flujo {
   
   public static void metodo1(){
     System.out.println("Ini do metodo1");
-    metodo2();
+
+    try {
+      metodo2();
+    } catch (MiException me) {
+      me.printStackTrace();
+    }
+    
     System.out.println("Fim do metodo1");
   }
 
   public static void metodo2(){
     System.out.println("Ini do metodo2");
-    throw new ArithmeticException(); // Aqui rompe todo y no debe haber nada abajo
-    // Las expeciones tambien son objetos
-    // throw solo se puede usar con objetos de excepciones
+    throw new MiException("Mi excepcion fue lanzada"); 
   }
   
 }
