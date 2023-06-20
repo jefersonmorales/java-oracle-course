7d73824cef62ea67c3c756f8764008f28426ea6a
jefersonmorales-l <jefersmorales@gmail.com>
2023-06-19 22:30:17 -0600

    lanzando excepciones

--- java-pila-ejecucion/src/Flujo.java
+++ java-pila-ejecucion/src/Flujo.java
@@ -14,33 +14,9 @@ public class Flujo {
 
   public static void metodo2(){
     System.out.println("Ini do metodo2");
-  
-    for(int i =1; i<= 5; i++){
-      System.out.println(i);
-
-      try {
-
-        if (i == 3) {
-          int num = 0;
-          int resultado = 20 / num; // Esto es un error porque cualquier numero dividido entre 0 es un error.
-          System.out.println(resultado);
-        }
-
-        String test = null;
-        System.out.println(test.toString());
-
-        // Podemos capturar diferentes errores, haciendo diferentes catch
-
-        // | Nos sirve para integrar diferentes errores
-      } catch(ArithmeticException | NullPointerException exception) { // Esto permite capturar el error
-        System.out.println("Atrapo error: " + exception.getMessage()); // nos retorna el mensaje
-        exception.printStackTrace(); // nos retorna la direccion de error
-      } 
-
-    }
-    
-    System.out.println("Fim do metodo2");
-  
+    throw new ArithmeticException(); // Aqui rompe todo y no debe haber nada abajo
+    // Las expeciones tambien son objetos
+    // throw solo se puede usar con objetos de excepciones
   }
   
 }
