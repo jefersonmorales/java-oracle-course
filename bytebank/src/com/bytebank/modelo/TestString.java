12d38a40ce3061244927f583f6a44921e96c3960
jefersonmorales-l <jefersmorales@gmail.com>
2023-07-06 13:14:51 -0600

    metodos de String

--- bytebank/src/com/bytebank/modelo/TestString.java
+++ bytebank/src/com/bytebank/modelo/TestString.java
@@ -3,16 +3,27 @@ package com.bytebank.modelo;
 public class TestString {
 	
 	public static void main(String[] args) {
-		String nombre = "Alura";
+		String nombre = "Jeferson";
+	
+		System.out.println("Sin cambios: " + nombre);
 		
-		System.out.println("Sin cambios :" + nombre);
-		
-		nombre =  nombre.concat(" curso");
+		nombre =  nombre.concat(" Morales");
 		System.out.println("Nombre concatenado: " + nombre);		
 		
-		String replace = nombre.replace("A", "a");
-		System.out.println(replace);
+		nombre = nombre.replace("Jeferson", "Steven");
+		System.out.println("Reemplazando caracter: " + nombre);
 		
+		nombre = nombre.toUpperCase();
+		System.out.println("Todo en mayuscula: " + nombre);
+		
+		nombre = nombre.toLowerCase();
+		System.out.println("Todo en minuscula: " + nombre);
+		
+		char letra = nombre.charAt(3);
+		System.out.println("Letra del indice 3: " + letra);
+		
+		int index = nombre.indexOf("o");
+		System.out.println("Indice de la letra o: " + index);
 	}
 
 }
