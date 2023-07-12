b0fc5e341f9b67f15a161e3c5849469980bc0a58
jefersonmorales-l <jefersmorales@gmail.com>
2023-07-12 14:26:09 -0600

    otro metodo de hacer arrays

---
+++ bytebank/src/com/bytebank/test/TestArraysReferencias2.java
@@ -0,0 +1,13 @@
+package com.bytebank.test;
+
+public class TestArraysReferencias2 {
+    public static void main(String[] args) {
+
+      int array[] = {9,4,10,5,2};
+
+      for (int i = 0; i < array.length; i++) {
+          System.out.println("Indice: " + i + ", Valor: " + array[i]);
+      }
+      
+    }
+}
