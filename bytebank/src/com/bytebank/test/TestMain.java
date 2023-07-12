74c418064a7085c9214c684b4b3427ff1df9d087
jefersonmorales-l <jefersmorales@gmail.com>
2023-07-12 13:46:30 -0600

    Arrays

---
+++ bytebank/src/com/bytebank/test/TestMain.java
@@ -0,0 +1,17 @@
+package com.bytebank.test;
+
+public class TestMain {
+    public static void main(String[] args) {
+
+        int[] edades = new int[5];
+        edades[0] = 1;
+        edades[1] = 2;
+        edades[2] = 3;
+        edades[3] = 4;
+        edades[4] = 5;
+
+        int indice = 3;
+        
+        System.out.println("Indice: " + indice + ". Valor: " + edades[indice]);
+    }
+}
