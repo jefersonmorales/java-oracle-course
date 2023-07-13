56439274be71ad989a5a8941885ca37c570b30b0
jefersonmorales-l <jefersmorales@gmail.com>
2023-07-12 22:24:05 -0600

    Metodo Main - entendiendo args

--- bytebank/src/com/bytebank/test/TestMain.java
+++ bytebank/src/com/bytebank/test/TestMain.java
@@ -3,6 +3,10 @@ package com.bytebank.test;
 public class TestMain {
     public static void main(String[] args) {
 
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+
         int[] edades = new int[5];
         edades[0] = 1;
         edades[1] = 2;
@@ -11,8 +15,7 @@ public class TestMain {
         edades[4] = 5;
 
         int indice = 3;
-        
-        
+
         int longitudArrays = edades.length;
 
         System.out.println("Indice: " + indice + ". Valor: " + edades[indice]);
