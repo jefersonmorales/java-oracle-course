2eb2140dbff7813678f73633a11ef3b34d26a587
jefersonmorales-l <jefersmorales@gmail.com>
2023-07-12 21:59:03 -0600

    cast objectos

--- bytebank/src/com/bytebank/test/TestArraysReferencias.java
+++ bytebank/src/com/bytebank/test/TestArraysReferencias.java
@@ -7,7 +7,7 @@ import com.bytebank.modelo.CuentaCorriente;
 public class TestArraysReferencias {
     public static void main(String[] args) {
 
-        CuentaAhorros cuentaAhorros1 = new CuentaAhorros(300); // instancia de una cuenta de ahorros # 1
+        CuentaCorriente cuentaAhorros1 = new CuentaCorriente(300); // instancia de una cuenta de ahorros # 1
 
         Cuenta[] cuentas = new Cuenta[5]; // creacion de array
         cuentas[1] = cuentaAhorros1; // agregando referencia en array
@@ -16,6 +16,9 @@ public class TestArraysReferencias {
         cuentas[3] = cuentaAhorros2; // agregando referencia en array
 
         cuentas[0] = new CuentaCorriente(100); // instancia de una cuenta corriente y agregando al array
+
+        CuentaCorriente cuenta = (CuentaCorriente) cuentas[1];
+        System.out.println(cuenta);
 
         for (int i = 0; i < cuentas.length; i++) {
             System.out.println("Indice # : " + i + ", Valor: " + cuentas[i]);
