242da0c4958d3bda7c91ca11bdf39da4648410e5
jefersonmorales-l <jefersmorales@gmail.com>
2023-07-13 12:39:09 -0600

    <>generic ArrayList

--- bytebank/src/com/bytebank/test/TestArrayList.java
+++ bytebank/src/com/bytebank/test/TestArrayList.java
@@ -1,5 +1,6 @@
 package com.bytebank.test;
 
+import com.bytebank.modelo.Cliente;
 import com.bytebank.modelo.Cuenta;
 import com.bytebank.modelo.CuentaCorriente;
 import com.bytebank.modelo.CuentaAhorros;
@@ -9,21 +10,29 @@ public class TestArrayList {
 
   public static void main(String[] args) {
 
-    ArrayList lista = new ArrayList();
+    // <> (Generics) Forzando a que solo acepte solo un tipo de objeto
+    ArrayList<Cuenta> lista = new ArrayList<>();
     Cuenta cc = new CuentaCorriente(1122);
     Cuenta ca = new CuentaAhorros(12321);
 
     lista.add(cc);
     lista.add(ca);
 
-    Cuenta obtenerCuenta = (Cuenta) lista.get(0);
-    Cuenta obtenerCuenta2 = (Cuenta) lista.get(1);
+    // ya no es necesario castear porque ya estamos indicando que va a usar Cuenta
+    Cuenta obtenerCuenta = lista.get(0);
+    Cuenta obtenerCuenta2 = lista.get(1);
 
     System.out.println(obtenerCuenta);
     System.out.println(obtenerCuenta2);
-    // System.out.println(lista.get(0));
-    // System.out.println(lista.get(1));
+
+    // for (int i = 0; i < lista.size(); i++) {
+    // System.out.println(lista.get(i));
+    // }
+
+    // for each
+    for (Cuenta cuenta : lista) {
+      System.out.println(cuenta);
+    }
 
   }
-
 }
