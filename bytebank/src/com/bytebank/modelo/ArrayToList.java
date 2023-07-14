371cc7aa7101dc4149540345655f0946e836b26a
jefersonmorales-l <jefersmorales@gmail.com>
2023-07-13 23:34:01 -0600

    arr to list

---
+++ bytebank/src/com/bytebank/modelo/ArrayToList.java
@@ -0,0 +1,15 @@
+package com.bytebank.modelo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayToList {
+
+  public static void main(String[] args) {
+
+    List<String> argumentos = Arrays.asList(args);
+
+  }
+
+}
